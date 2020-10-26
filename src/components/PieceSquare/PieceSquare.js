531a687367fc6fb84457040cc380a77e31f59f99
Leavemydogalone <niklasport88@gmail.com>
2020-10-26 09:47:26 -0700

    got the color change to work on ship death

--- src/components/PieceSquare/PieceSquare.js
+++ src/components/PieceSquare/PieceSquare.js
@@ -2,14 +2,11 @@ import React, { useEffect } from 'react';
 
 // to serve as building block for ships
 export default function PieceSquare({ direction, isSet, id, isdead }) {
-  let backGround = isSet
-    ? { backgroundColor: 'grey' }
-    : { backgroundColor: 'blue' };
-  // console.log(isdead);
-  useEffect(() => {
-    // if (isdead === 'true') backGround = { backgroundColor: 'red' };
-    console.log(isdead);
-  }, [isdead]);
+  const backGround = !isSet
+    ? { backgroundColor: 'blue' }
+    : isdead === 'true'
+    ? { backgroundColor: 'red' }
+    : { backgroundColor: 'grey' };
 
   const theStyle =
     direction === 'down'
--- src/components/Ship/Ship.js
+++ src/components/Ship/Ship.js
@@ -5,17 +5,10 @@ import PieceSquare from 'components/PieceSquare/PieceSquare.js';
 export default function Ship({ size, name, direction, shipInfo, handleClick }) {
   const shipArr = new Array(size).fill(name);
   let directionStyle;
-  const [isDead, setIsDead] = useState(false);
+  const isDead = shipInfo.isDead ? 'true' : 'false';
 
   if (direction === 'down')
     directionStyle = { display: 'inline-block', marginRight: '5px', top: 0 };
-
-  useEffect(() => {
-    if (shipInfo.isDead) {
-      setIsDead(true);
-      console.log(shipInfo.name + ' is dead');
-    }
-  }, [shipInfo]);
 
   const theShip = shipArr.map((thing, index) => (
     <PieceSquare
@@ -23,6 +16,7 @@ export default function Ship({ size, name, direction, shipInfo, handleClick }) {
       direction={direction}
       isSet={shipInfo.isSet}
       id={name}
+      isdead={isDead.toString()}
     />
   ));
 
@@ -32,7 +26,6 @@ export default function Ship({ size, name, direction, shipInfo, handleClick }) {
       className="ship"
       style={directionStyle}
       onClick={shipInfo.isSet ? null : handleClick}
-      isdead={isDead.toString()}
     >
       {theShip}
     </div>
